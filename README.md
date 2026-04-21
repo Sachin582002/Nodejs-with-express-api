aa219d26adf7afa70c239e6ba3700b46122f713e
sachin kunwar <sachinkunwar284@gmail.com>
2026-04-21 11:15:49 +0545

    init express

---
+++ README.md
@@ -0,0 +1,39 @@
+# Node with Express API
+
+## Node.Js
+
+- Node.js is a JavaScript runtime.
+- Runtime: A program that runs another program.
+- Run JS in local machine.
+- Built on C++
+- Powered by Google Chrome v8 engine
+- Used to build: API, real time apps, micro-services
+
+## Architecture
+
+- Single threaded
+- Non-blocking I/O operation
+- Event loop
+
+## Express.js
+
+- It is a Node.js API/backend framework.
+- Used to build API (Application program interface)
+- It simplies the HTTP module of node.js
+- Minimalist, fast and unopinionated framework
+
+## API
+
+- API format: JSON (JavaScript Object Notation)
+- REST API (Representational state transfer)
+
+### JSON
+
+- JS Object => JSON.stringify() => JSON
+- JSON => JSON.parse() => JS Object
+
+=========================
+
+- HTTP Methods
+- Layered architecture
+- Postman
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+import fs from "fs/promises";
+import config from "./config/config.js";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Page");
+});
+
+app.post("/contact", (req, res) => {
+  res.send("Contact form submitted.");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await fs.readFile("src/data/products.json", "utf8");
+
+  res.json(JSON.parse(products));
+});
+
+app.get("/products/first", async (req, res) => {
+  const products = await fs.readFile("src/data/products.json", "utf8");
+
+  const firstProduct = JSON.parse(products)[0];
+
+  res.json(firstProduct);
+});
+
+app.listen(config.port, () => {
+  console.log(`Server running at port ${config.port}...`);
+});
